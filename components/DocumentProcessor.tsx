627c5fd5f506a7dcd8bdf7b129113f32846cde39
Hakeem Kasali <keem.kasali@gmail.com>
2025-06-26 13:51:15 +0100

    Updated DocumentProcessor.tsx

--- components/DocumentProcessor.tsx
+++ components/DocumentProcessor.tsx
@@ -284,7 +284,7 @@ export function DocumentProcessor({
                 onVoiceToggle={() => {}}
                 placeholder="Paste the job description here..."
                 mode="document"
-                maxLength={3000}
+                maxLength={10000}
                 suggestions={jobDescriptionSuggestions}
                 showSuggestions={!jobDescription.trim()}
               />
